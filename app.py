9c6c757f6248150189ca0b796f98409ba06ccfba
SATYA JAYANTH <jkotikel@gmail.com>
2025-12-26 16:42:27 +0530

    Refactor device control routes to accept device names

--- app.py
+++ app.py
@@ -7,26 +7,25 @@ app = Flask(__name__)
 SUPABASE_URL = "https://ofyhamnfkpgtnujmqgiv.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9meWhhbW5ma3BndG51am1xZ2l2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2NTM4MzIyMiwiZXhwIjoyMDgwOTU5MjIyfQ.YLtXejHgDlLr1es0suj06eP1-WUp7kBriaLgSVf37Ds"
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
-
-
-
 @app.route("/")
 def index():
     data = supabase.table("devices").select("*").execute()
     return render_template("index.html", devices=data.data)
 
-@app.route("/on")
-def on():
+@app.route("/on/<device_name>")
+def on(device_name):
+    print("ON clicked for:", device_name)   # 
     supabase.table("devices").update(
         {"status": True}
-    ).eq("device_name", "light1").execute()
+    ).eq("device_name", device_name).execute()
     return redirect(url_for("index"))
 
-@app.route("/off")
-def off():
+@app.route("/off/<device_name>")
+def off(device_name):
+    print("OFF clicked for:", device_name)  # 🔴 DEBUG
     supabase.table("devices").update(
         {"status": False}
-    ).eq("device_name", "light1").execute()
+    ).eq("device_name", device_name).execute()
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
